e4e7cdbb48b595317468c84f8a9b95f4e72c3967
WeegieCat <weegieluke@gmail.com>
2026-03-04 21:11:53 +0900

    初期トライ木の表示位置を下に調整

--- src/components/SceneContent.tsx
+++ src/components/SceneContent.tsx
@@ -62,7 +62,7 @@ export function SceneContent() {
 
     return (
         <>
-            <PerspectiveCamera makeDefault position={[0, 7, 18]} />
+            <PerspectiveCamera makeDefault position={[0, 20, 30]} />
             <OrbitControls />
 
             {/* ライティング */}
